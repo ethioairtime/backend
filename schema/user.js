2ac87d82ee58cc645d494c4960da27e8e7343ef4
bereket-kassahun <beckykassahun@gmail.com>
2025-10-23 16:07:18 +0300

    minor bug fix

--- schema/user.js
+++ schema/user.js
@@ -3,8 +3,6 @@ const mongoose = require("mongoose");
 // User schema and model
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
     phone_number: { type: String, required: true },
     wallet: { type: Number, default: 0 }
 });

228fcf6f113827cf3712e200776104e7f264a500
bereket-kassahun <beckykassahun@gmail.com>
2025-11-30 10:40:59 +0300

    making phone_number not unique in sms queue

--- schema/cbe_queue.js
+++ schema/cbe_queue.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cbeQueueSchema = new mongoose.Schema({
     reference_number: { type: String, required: true, unique: true },
     amount: { type: Number, required: true },
-    phone_number:  { type: String, unique: true },
+    phone_number:  { type: String },
     paid: { type: Boolean, default: false },
     verified: { type: Boolean, default: false },
     created_at: { type: Date, default: Date.now }
